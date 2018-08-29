2b2efdc1a9ce4b56cc28336322116c5db444797c
morihaya <yukiya.hayashi@gmail.com>
2018-08-30 01:18:41 +0900

    feat: Google Oauth2 login

--- main.go
+++ main.go
@@ -1,15 +1,206 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"os"
 	"os/exec"
+	"sort"
 	"time"
 
+	"github.com/gorilla/securecookie"
 	"github.com/kataras/iris"
 	"github.com/kataras/iris/middleware/basicauth"
 	"github.com/kataras/iris/middleware/logger"
 	"github.com/kataras/iris/middleware/recover"
+	"github.com/kataras/iris/sessions"
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/providers/gplus"
 )
+
+type ProviderIndex struct {
+	Providers    []string
+	ProvidersMap map[string]string
+}
+
+var sessionsManager *sessions.Sessions
+
+func init() {
+	// attach a session manager
+	cookieName := "mycustomsessionid"
+	// AES only supports key sizes of 16, 24 or 32 bytes.
+	// You either need to provide exactly that amount or you derive the key from what you type in.
+	hashKey := []byte("the-big-and-secret-fash-key-here")
+	blockKey := []byte("lot-secret-of-characters-big-too")
+	secureCookie := securecookie.New(hashKey, blockKey)
+
+	sessionsManager = sessions.New(sessions.Config{
+		Cookie: cookieName,
+		Encode: secureCookie.Encode,
+		Decode: secureCookie.Decode,
+	})
+}
+
+// These are some function helpers that you may use if you want
+
+// GetProviderName is a function used to get the name of a provider
+// for a given request. By default, this provider is fetched from
+// the URL query string. If you provide it in a different way,
+// assign your own function to this variable that returns the provider
+// name for your request.
+var GetProviderName = func(ctx iris.Context) (string, error) {
+	// try to get it from the url param "provider"
+	if p := ctx.URLParam("provider"); p != "" {
+		return p, nil
+	}
+
+	// try to get it from the url PATH parameter "{provider} or :provider or {provider:string} or {provider:alphabetical}"
+	if p := ctx.Params().Get("provider"); p != "" {
+		return p, nil
+	}
+
+	// try to get it from context's per-request storage
+	if p := ctx.Values().GetString("provider"); p != "" {
+		return p, nil
+	}
+	// if not found then return an empty string with the corresponding error
+	return "", errors.New("you must select a provider")
+}
+
+/*
+BeginAuthHandler is a convenience handler for starting the authentication process.
+It expects to be able to get the name of the provider from the query parameters
+as either "provider" or ":provider".
+
+BeginAuthHandler will redirect the user to the appropriate authentication end-point
+for the requested provider.
+
+See https://github.com/markbates/goth/examples/main.go to see this in action.
+*/
+func BeginAuthHandler(ctx iris.Context) {
+	url, err := GetAuthURL(ctx)
+	if err != nil {
+		ctx.StatusCode(iris.StatusBadRequest)
+		ctx.Writef("%v", err)
+		return
+	}
+
+	ctx.Redirect(url, iris.StatusTemporaryRedirect)
+}
+
+/*
+GetAuthURL starts the authentication process with the requested provided.
+It will return a URL that should be used to send users to.
+
+It expects to be able to get the name of the provider from the query parameters
+as either "provider" or ":provider" or from the context's value of "provider" key.
+
+I would recommend using the BeginAuthHandler instead of doing all of these steps
+yourself, but that's entirely up to you.
+*/
+func GetAuthURL(ctx iris.Context) (string, error) {
+	providerName, err := GetProviderName(ctx)
+	if err != nil {
+		return "", err
+	}
+
+	provider, err := goth.GetProvider(providerName)
+	if err != nil {
+		return "", err
+	}
+	sess, err := provider.BeginAuth(SetState(ctx))
+	if err != nil {
+		return "", err
+	}
+
+	url, err := sess.GetAuthURL()
+	if err != nil {
+		return "", err
+	}
+	session := sessionsManager.Start(ctx)
+	session.Set(providerName, sess.Marshal())
+	return url, nil
+}
+
+// SetState sets the state string associated with the given request.
+// If no state string is associated with the request, one will be generated.
+// This state is sent to the provider and can be retrieved during the
+// callback.
+var SetState = func(ctx iris.Context) string {
+	state := ctx.URLParam("state")
+	if len(state) > 0 {
+		return state
+	}
+
+	return "state"
+
+}
+
+// GetState gets the state returned by the provider during the callback.
+// This is used to prevent CSRF attacks, see
+// http://tools.ietf.org/html/rfc6749#section-10.12
+var GetState = func(ctx iris.Context) string {
+	return ctx.URLParam("state")
+}
+
+/*
+CompleteUserAuth does what it says on the tin. It completes the authentication
+process and fetches all of the basic information about the user from the provider.
+
+It expects to be able to get the name of the provider from the query parameters
+as either "provider" or ":provider".
+
+See https://github.com/markbates/goth/examples/main.go to see this in action.
+*/
+var CompleteUserAuth = func(ctx iris.Context) (goth.User, error) {
+	providerName, err := GetProviderName(ctx)
+	if err != nil {
+		return goth.User{}, err
+	}
+
+	provider, err := goth.GetProvider(providerName)
+	if err != nil {
+		return goth.User{}, err
+	}
+	session := sessionsManager.Start(ctx)
+	value := session.GetString(providerName)
+	if value == "" {
+		return goth.User{}, errors.New("session value for " + providerName + " not found")
+	}
+
+	sess, err := provider.UnmarshalSession(value)
+	if err != nil {
+		return goth.User{}, err
+	}
+
+	user, err := provider.FetchUser(sess)
+	if err == nil {
+		// user can be found with existing session data
+		return user, err
+	}
+
+	// get new token and retry fetch
+	_, err = sess.Authorize(provider, ctx.Request().URL.Query())
+	if err != nil {
+		return goth.User{}, err
+	}
+
+	session.Set(providerName, sess.Marshal())
+	return provider.FetchUser(sess)
+}
+
+// Logout invalidates a user session.
+func Logout(ctx iris.Context) error {
+	providerName, err := GetProviderName(ctx)
+	if err != nil {
+		return err
+	}
+	session := sessionsManager.Start(ctx)
+	session.Delete(providerName)
+	return nil
+}
+
+// End of the "some function helpers".
 
 func newApp() *iris.Application {
 	app := iris.New()
@@ -20,6 +211,66 @@ func newApp() *iris.Application {
 	// and log the requests to the terminal.
 	app.Use(recover.New())
 	app.Use(logger.New())
+
+	goth.UseProviders(
+		gplus.New(os.Getenv("GPLUS_KEY"), os.Getenv("GPLUS_SECRET"), "http://localhost:8080/auth/gplus/callback"),
+	)
+
+	m := make(map[string]string)
+	m["gplus"] = "Google Plus"
+
+	var keys []string
+	for k := range m {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	providerIndex := &ProviderIndex{Providers: keys, ProvidersMap: m}
+
+	// attach and build our templates
+	app.RegisterView(iris.HTML("./templates", ".html"))
+
+	// start of the router
+
+	app.Get("/auth/{provider}/callback", func(ctx iris.Context) {
+
+		user, err := CompleteUserAuth(ctx)
+		if err != nil {
+			ctx.StatusCode(iris.StatusInternalServerError)
+			ctx.Writef("%v", err)
+			return
+		}
+		ctx.ViewData("", user)
+		if err := ctx.View("user.html"); err != nil {
+			ctx.Writef("%v", err)
+		}
+	})
+
+	app.Get("/logout/{provider}", func(ctx iris.Context) {
+		Logout(ctx)
+		ctx.Redirect("/", iris.StatusTemporaryRedirect)
+	})
+
+	app.Get("/auth/{provider}", func(ctx iris.Context) {
+		// try to get the user without re-authenticating
+		if gothUser, err := CompleteUserAuth(ctx); err == nil {
+			ctx.ViewData("", gothUser)
+			if err := ctx.View("user.html"); err != nil {
+				ctx.Writef("%v", err)
+			}
+		} else {
+			BeginAuthHandler(ctx)
+		}
+	})
+
+	app.Get("/", func(ctx iris.Context) {
+
+		ctx.ViewData("", providerIndex)
+
+		if err := ctx.View("index.html"); err != nil {
+			ctx.Writef("%v", err)
+		}
+	})
 
 	authConfig := basicauth.Config{
 		Users:   map[string]string{"myusername": "mypassword", "mySecondusername": "mySecondpassword"},
@@ -54,9 +305,9 @@ func newApp() *iris.Application {
 
 	// Method:   GET
 	// Resource: http://localhost:8080
-	app.Handle("GET", "/", func(ctx iris.Context) {
-		ctx.HTML("<h1>Welcome</h1>")
-	})
+	//	app.Handle("GET", "/", func(ctx iris.Context) {
+	//		ctx.HTML("<h1>Welcome</h1>")
+	//	})
 
 	// same as app.Handle("GET", "/ping", [...])
 	// Method:   GET
